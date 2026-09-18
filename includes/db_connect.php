b2f6f695673c7b4ec8b9e5bfab00ddb30ffd2911
GabVx-cmd <gabzdeleon1001@gmail.com>
2026-09-18 14:38:15 +0800

    Change the load to safeLoad to prevent server crash in azure

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -19,7 +19,7 @@ require_once __DIR__ . '/../classes/leave_request.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->load();
+$dotenv->sageLoad();
 
 $database = new db();
 $conn = $database->getConnection();
